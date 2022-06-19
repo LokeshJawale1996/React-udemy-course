388683ef3fae6c8cd048bcadad5ed3754a08555d
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-19 17:28:25 +0530

    useState function including basics and fetch example

--- tutorials-react-advanced/src/App.js
+++ tutorials-react-advanced/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-import SetUp from "./tutorial/1-useState/setup/5-useState-counter";
+// import SetUp from "./tutorial/1-useState/setup/5-useState-counter";
+import UseEffect from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 
 function App() {
   return (
     <div className="container">
-      <SetUp />
+      {/* <SetUp /> */}
+      <UseEffect />
     </div>
   );
 }
