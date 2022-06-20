dcce58e5952d12785b42b9fc0c46f1fde465e325
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-20 13:27:13 +0530

    react router(useParams and NavLink

--- react-router-6-tutorial-main/src/pages/SingleProduct.js
+++ react-router-6-tutorial-main/src/pages/SingleProduct.js
@@ -1,7 +1,11 @@
+import { Link, useParams } from "react-router-dom";
 const SingleProduct = () => {
+  const { productId } = useParams();
+  console.log(productId);
   return (
-    <section className='section product'>
-      <h2>single product</h2>
+    <section className="section product">
+      <h2>{productId}</h2>
+      <Link to="/products">Back to products</Link>
     </section>
   );
 };
